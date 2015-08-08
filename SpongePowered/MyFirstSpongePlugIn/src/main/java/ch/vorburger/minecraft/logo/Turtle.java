09ea2b2edc79efa20b18254291f3eec2225a05a6
Michael Vorburger <mike@vorburger.ch>
2015-08-08 15:47:37 +0530

    OK, direction is working!

--- SpongePowered/MyFirstSpongePlugIn/src/main/java/ch/vorburger/minecraft/logo/Turtle.java
+++ SpongePowered/MyFirstSpongePlugIn/src/main/java/ch/vorburger/minecraft/logo/Turtle.java
@@ -1,6 +1,7 @@
 package ch.vorburger.minecraft.logo;
 
 import org.spongepowered.api.block.BlockType;
+import org.spongepowered.api.util.Direction;
 import org.spongepowered.api.world.Location;
 
 import com.flowpowered.math.vector.Vector3d;
@@ -10,35 +11,26 @@ public class Turtle {
 
 	// TODO How to, optionally, "connect" this to a Player??
 	
-	private final static Vector3i Vector3i0   = new Vector3i( 0, -1, 0);
-	private final static Vector3i Vector3i90  = new Vector3i(-1,  0, 0);
-	private final static Vector3i Vector3i180 = new Vector3i( 0,  1, 0);
-	private final static Vector3i Vector3i270 = new Vector3i( 1,  0, 0);
-	
 	Location location;
-	Vector3i direction;
+	Direction direction;
 	BlockType blockType;
 	boolean isSettingBlockOnMove = true;
 
 	Turtle(Location location, Vector3d rotation, BlockType blockType) {
-		this.location = new Location(location.getExtent(), location.getBlockPosition().add(getDirectionFromRotation(rotation)));
-		this.direction = Vector3i0; // getDirectionFromRotation(rotation);
+		this.location = getStartingLocation(location, rotation);
+		this.direction = getDirection(rotation);
 		this.blockType = blockType;
 	}
 	
-	private Vector3i getDirectionFromRotation(Vector3d rotation) {
-		// https://bukkit.org/threads/tutorial-how-to-calculate-vectors.138849/
-		int yaw = rotation.getFloorX(); // angle the player is rotating horizontally in, in degrees
-		int pitch = rotation.getFloorY(); // angle up/down the player is looking in, in degrees
-		
-		double pitchInRadian = ((pitch + 90) * Math.PI) / 180;
-		double yawInRadian   = (( yaw  + 90) * Math.PI) / 180;
-		
-		double x = Math.sin(pitchInRadian) * Math.cos(yawInRadian);
-		double y = Math.sin(pitchInRadian) * Math.sin(yawInRadian);
-		double z = Math.cos(pitchInRadian);
-		
-		return new Vector3i(x, y, z);
+	private Location getStartingLocation(Location location, Vector3d rotation) {
+		// TODO How to get a Location from where the Player is looking at?
+		// https://bukkit.org/threads/tutorial-how-to-calculate-vectors.138849/ ?
+		return location;
+	}
+
+	private Direction getDirection(Vector3d rotation) {
+		Direction initialDirection = Direction.getClosestHorizonal(rotation);
+		return initialDirection;
 	}
 
 	// ---
@@ -74,7 +66,7 @@ public class Turtle {
 
 	/** S */
 	void moveBack() {
-		move(direction.negate());
+		move(direction.getOpposite());
 	}
 
 	/** Space */
@@ -87,13 +79,12 @@ public class Turtle {
 		// TODO move(direction.negate());
 	}
 
-	private void move(Vector3i moveDirection) {
+	private void move(Direction directionToMove) {
 		setBlockIfPenDown();
 		Vector3i oldBlockPosition = location.getBlockPosition();
-System.out.println(oldBlockPosition);
-		Vector3i newBlockPosition = oldBlockPosition.add(moveDirection);
+		// TODO LOW For performance, it would be better if Direction class had a toVector3i 
+		Vector3i newBlockPosition = oldBlockPosition.add(directionToMove.toVector3d().toInt());
 		location = new Location(location.getExtent(), newBlockPosition);
-System.out.println(location);
 	}
 
 	private void setBlockIfPenDown() {
