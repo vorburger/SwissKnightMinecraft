01a3f7f195d0f6a343e680c64912609ad62ef099
Michael Vorburger <mike@vorburger.ch>
2015-09-14 15:44:29 +0200

    CommandRegistery can register CommandSpec directly, like CommandMapping

--- SpongePowered/MyFirstSpongePlugIn/src/main/java/ch/vorburger/minecraft/command/CommandRegistry.java
+++ SpongePowered/MyFirstSpongePlugIn/src/main/java/ch/vorburger/minecraft/command/CommandRegistry.java
@@ -1,19 +1,25 @@
 package ch.vorburger.minecraft.command;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import org.slf4j.Logger;
 import org.spongepowered.api.Game;
+import org.spongepowered.api.plugin.PluginContainer;
 import org.spongepowered.api.util.command.CommandMapping;
+import org.spongepowered.api.util.command.spec.CommandSpec;
 
+import com.google.common.base.Optional;
 import com.google.inject.Inject;
 
 public class CommandRegistry {
 
-	// private @Inject Logger logger;
+	protected @Inject Logger logger;
 	protected @Inject Game game;
-
+	protected @Inject PluginContainer plugin;
+	
 	private List<CommandMapping> commandMappings = new ArrayList<CommandMapping>();
 
 	synchronized public void register(CommandMapping commandMapping) {
@@ -39,6 +45,17 @@ public class CommandRegistry {
 	// @see org.spongepowered.api.util.command.dispatcher.Dispatcher.getCommands()
 	public List<CommandMapping> getCommands() {
 		return Collections.unmodifiableList(commandMappings);
+	}
+	
+	// ---
+	
+	public void register(CommandSpec commandSpec, String... commandNameAndAliases) {
+		Optional<CommandMapping> optionalCommandMapping = game.getCommandDispatcher().register(plugin, commandSpec, commandNameAndAliases);
+		if (optionalCommandMapping.isPresent()) {
+			register(optionalCommandMapping.get());
+		} else {
+			logger.warn("Could not register new command with name/aliases: " + Arrays.toString(commandNameAndAliases));
+		}
 	}
 
 }
