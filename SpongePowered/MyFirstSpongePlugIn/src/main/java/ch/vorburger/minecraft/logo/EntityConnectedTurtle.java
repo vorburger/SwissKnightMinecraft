af9e055ca772e54fe52040f54815f2c87546ef08
Michael Vorburger <mike@vorburger.ch>
2015-10-10 01:09:26 +0200

    EntityConnectedTurtle setLocation is more natural than setLocationSafely

--- SpongePowered/MyFirstSpongePlugIn/src/main/java/ch/vorburger/minecraft/logo/EntityConnectedTurtle.java
+++ SpongePowered/MyFirstSpongePlugIn/src/main/java/ch/vorburger/minecraft/logo/EntityConnectedTurtle.java
@@ -5,7 +5,14 @@ import org.spongepowered.api.util.Direction;
 import org.spongepowered.api.world.Location;
 
 /**
- * {@link Turtle} that is connected to a visible Entity, moving that Entity along.
+ * {@link Turtle} that is connected to a visible Entity, moving that Entity
+ * along.
+ * 
+ * Note that an Entity with AI such as a Pig will tend to turn and move away,
+ * and is thus not very suitable as a Turtle. A Human is a bit better, but also
+ * not perfectly ideal because it obviously obeys gravity and thus falls (TODO
+ * How-To make it Fly??). Also when the Turtle is within existing blocks
+ * rendering can be weired.
  *
  * @author Michael Vorburger
  */
@@ -20,7 +27,11 @@ public class EntityConnectedTurtle extends Turtle {
 
 	@Override
 	protected void onMove(Location newLocation) {
-		companion.setLocationSafely(newLocation);
+		// setLocation is better/clearer than setLocationSafely, for a Turtle -
+		// even if it can lead to weired visuals of Entity-in-Block, but that's
+		// still preferable to the alternative of the Companion Entity not
+		// showing clearly where the Turtle is.
+		companion.setLocation(newLocation);
 	}
 	
 	@Override
